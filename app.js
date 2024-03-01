385517de244b9edee65fd3b00358eea5a474e7b1
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-29 23:25:54 -0600

    added

---
+++ app.js
@@ -0,0 +1,13 @@
+let bodyInfoLinks = document.querySelectorByClassName("body-info-links");
+let bodyContents = document.querySelectorByClassName("body-content");
+function opentab(tabname){
+    for(let bodyInfoLink of bodyInfoLinks){
+        bodyInfoLink.classList.remove("active-link");
+
+        for(bodyContent of bodyContents){
+            bodyContent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    }
+}
