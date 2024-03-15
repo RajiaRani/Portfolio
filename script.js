5f1f2553a0475fe2c31f6143ffabe35b7ee100ce
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-14 21:29:08 -0500

    added

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-function showSection(sectionId) {
-    // Hide all sections and reset opacity
-    document.querySelectorAll('.content-section').forEach(function(section) {
-        section.style.display = 'none';
-        section.style.opacity = 0;
-    });
 
-    // Use setTimeout to allow the display change to take effect before fading in
-    const selectedSection = document.getElementById(sectionId);
-    selectedSection.style.display = 'block';
-    setTimeout(() => selectedSection.style.opacity = 1, 10); // Short delay before starting the opacity transition
-}
 
 
 
