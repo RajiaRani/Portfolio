d20162ee6fe8f892fbcada2c3f0e4ca861845a7e
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-20 13:42:42 -0500

    added

--- js/script.js
+++ js/script.js
@@ -6,6 +6,24 @@ let typed = new Typed(".typing", {
     loop : true,
 });
 
+
+
+
+/* ----------------------- Toggle Navbar ------------------------------- */
+let navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+});
+function  hideSection(){
+    document.querySelector("section.active").classList.toggle("fade-out");
+};
+function toggleNavbar(){
+    document.querySelector(".header").classList.toggle("active");
+}
+
+
 /* =============================== 
     Active Section 
  ============================================= */
