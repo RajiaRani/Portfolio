e8d2b2b46cc1306f9f730151124c4c5efb52c397
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-20 12:28:42 -0500

    added

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@ let typed = new Typed(".typing", {
     BackSpeed : 60,
     loop : true,
 });
+
+/* =============================== 
+    Active Section 
+ ============================================= */
+document.addEventListener("click", (e)=> {
+    if(e.target.classList.contains("link-item")&& e.target.hash !== ""){
+        const hash = e.target.hash;
+        //console.log(hash);
+        document.querySelector(".overlay").classList.add("active"); // activate overlay to prevent multiple clicks
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")){
+            //console.log("true");
+            toggleNavbar();
+        } else {
+            //console.log("false");
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(()=> {
+            document.querySelector("section.active").classList.remove("active", "fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+        }, 500)
+    }
+});
