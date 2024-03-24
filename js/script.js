b86740054bca35f00bdda4197e7fef5af5a066f1
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-23 21:39:08 -0500

    added

--- js/script.js
+++ js/script.js
@@ -56,3 +56,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+
+
+  /* =======================================
+   * Navbar links active state on scroll
+   ========================================== */
+   const navbarlinks = document.querySelectorAll("#navbar .scrollto");
+  const updateNavbarLinksActive = () =>{
+    let position = window.scrollY+200;
+
+    navbarlinks.forEach( link => {
+      const sectionSelector = link.getAttribute("href");
+      if( !sectionSelector.startsWith("#") || sectionSelector === "#"){
+        return;
+      }
+      const section = document.querySelector(sectionSelector);
+      if(!section){
+        return;
+      }
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    });
+  };
+// Attach the updateNavbarLinksActive function to scroll and load events
+window.addEventListener('scroll', updateNavbarLinksActive);
+window.addEventListener('load', updateNavbarLinksActive);
