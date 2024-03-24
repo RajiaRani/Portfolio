4b1dcefb8d6c91896d7e5291d473509769cc294c
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-23 22:58:37 -0500

    added

--- js/script.js
+++ js/script.js
@@ -9,56 +9,46 @@ let typed = new Typed(".typing", {
 
 
 /* ----------------------- About Tabs --------------------- */
-const tabContainer = document.querySelector(".about-tabs");
-const skillContainer = document.querySelector(".about-skills-tabs");
+const tabContainer = document.querySelectorAll(".about-tabs");
 const aboutSection = document.querySelector(".about-section");
-
-tabContainer.addEventListener("click", handleTabClick);
-skillContainer.addEventListener("click", handleSkillContainer);
 
 function handleTabClick(e) {
   if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active") ) {
-    tabContainer.querySelector(".active").classList.remove("active");
+    e.currentTarget.querySelector(".tab-item.active").classList.remove("active");
     e.target.classList.add("active");
     const target = e.target.getAttribute("data-target");
+    const aboutSection = document.querySelector(".about-section");
     aboutSection.querySelector(".tab-content.active").classList.remove("active");
     aboutSection.querySelector(target).classList.add("active");
   }
 }
-function handleSkillContainer(e) {
-  if (e.target.classList.contains("tab-skill-item") && !e.target.classList.contains("active")) {
-    skillContainer.querySelector(".active").classList.remove("active");
-    e.target.classList.add("active");
-    const target = e.target.getAttribute("data-target");
-    aboutSection.querySelector(".skill-tab-content.active").classList.remove("active");
-    aboutSection.querySelector(target).classList.add("active");
-  }
-}
 
-
+tabContainer.forEach(container => {
+  container.addEventListener("click", handleTabClick);
+});
 
 
 
 /* ============================== SEE MORE FUNCTION =============================== */
-// document.addEventListener('DOMContentLoaded', function () {
-//   // Attach click event listeners to all 'see-more-link' elements
-//   document.querySelectorAll('.see-more-link').forEach(function(link) {
-//       link.addEventListener('click', function(event) {
-//           event.preventDefault(); // Prevent the default anchor link behavior
+ document.addEventListener('DOMContentLoaded', function () {
+   // Attach click event listeners to all 'see-more-link' elements
+  document.querySelectorAll('.see-more-link').forEach(function(link) {
+       link.addEventListener('click', function(event) {
+           event.preventDefault(); // Prevent the default anchor link behavior
 
-//           // Find the nearest ancestor of the link which is a 'timeline-item'
-//           const timelineItem = this.closest('.timeline-item');
-//           // Within this 'timeline-item', find the '.more-text' element to toggle
-//           const moreText = timelineItem.querySelector('.more-text');
+           // Find the nearest ancestor of the link which is a 'timeline-item'
+           const timelineItem = this.closest('.timeline-item');
+           // Within this 'timeline-item', find the '.more-text' element to toggle
+           const moreText = timelineItem.querySelector('.more-text');
 
-//           // Toggle the visibility of the '.more-text' content
-//           if (moreText.style.display === 'none' || moreText.style.display === '') {
-//               moreText.style.display = 'block'; // Show the content
-//               this.textContent = 'See Less'; // Optionally change the link text to 'See Less'
-//           } else {
-//               moreText.style.display = 'none'; // Hide the content
-//               this.textContent = 'See More'; // Change back the link text to 'See More'
-//           }
-//       });
-//   });
-// });
+           // Toggle the visibility of the '.more-text' content
+           if (moreText.style.display === 'none' || moreText.style.display === '') {
+              moreText.style.display = 'block'; // Show the content
+               this.textContent = 'See Less'; // Optionally change the link text to 'See Less'
+           } else {
+               moreText.style.display = 'none'; // Hide the content
+               this.textContent = 'See More'; // Change back the link text to 'See More'
+           }
+      });
+   });
+ });
