3825ad56b486e03c38f66e44d9694ed6b4a1cc67
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-26 13:57:44 -0500

    addded

--- js/script.js
+++ js/script.js
@@ -8,6 +8,27 @@ let typed = new Typed(".typing", {
 
 
 
+// Get all "See More" buttons
+document.addEventListener('DOMContentLoaded', function () {
+  const seeMoreButtons = document.querySelectorAll('.seeMore');
+
+  seeMoreButtons.forEach(button => {
+      button.addEventListener('click', function () {
+          const moreContent = this.previousElementSibling;
+          if (moreContent.style.display === 'none' || moreContent.style.display === '') {
+              moreContent.style.display = 'block';
+              this.textContent = 'See Less';
+          } else {
+              moreContent.style.display = 'none';
+              this.textContent = 'See More';
+          }
+      });
+  });
+});
+
+
+
+
 
 
 
